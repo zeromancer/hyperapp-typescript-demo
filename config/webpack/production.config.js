0fe5ea340f6146cc551e5bec6fad84b6a6080961
David Siewert <david0siewert@gmail.com>
2018-02-11 21:48:07 +0100

    fix production build

--- config/webpack/production.config.js
+++ config/webpack/production.config.js
@@ -4,22 +4,27 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
 const commonConfig = require("./common.config")
 
+const extractSass = new ExtractTextPlugin({
+  filename: "[name].[contenthash].css",
+  disable: process.env.NODE_ENV === "development",
+})
+
 module.exports = merge(commonConfig, {
   module: {
     rules: [
       {
         test: /\.(s?css|sass)$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+        use: extractSass.extract({
           use: [
             {
               loader: "css-loader",
-              options: {
-                modules: true,
-              },
             },
-            { loader: "sass-loader" },
+            {
+              loader: "sass-loader",
+            },
           ],
+          // use style-loader in development
+          fallback: "style-loader",
         }),
       },
     ],
