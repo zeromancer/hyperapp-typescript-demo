ddf493928fa41a1bdfad8047e9ba61be6b8169d2
David Siewert <david0siewert@gmail.com>
2018-02-13 21:36:50 +0100

    fix: css classes in view

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,7 +7,7 @@ interface CounterProps {
 }
 
 const Counter: Component<CounterProps> = ({ count, onChange }: CounterProps) => (
-  <div>
+  <div class="container">
     <div class="content">
       <h1>counter</h1>
       <h4>simples hyperapp example, without local storage</h4>
